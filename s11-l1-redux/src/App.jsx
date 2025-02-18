030fe3c849a69f16bf4e8f86145c8eb6dfc19df8
lorella-marino <lorellamarino@gmail.com>
2025-02-18 11:12:14 +0100

    caricamento 17.02

--- s11-l1-redux/src/App.jsx
+++ s11-l1-redux/src/App.jsx
@@ -4,10 +4,12 @@ import MainSearch from "./components/MainSearch";
 import CompanySearchResults from "./components/CompanySearchResults";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Favourites from "./components/Favourites";
+import MyNav from "./components/MyNav";
 
 function App() {
   return (
     <BrowserRouter>
+      <MyNav />
       <Routes>
         <Route path="/" element={<MainSearch />} />
         <Route path="/:companyName" element={<CompanySearchResults />} />
--- s11-l1-redux/src/components/Favourites.jsx
+++ s11-l1-redux/src/components/Favourites.jsx
@@ -1,20 +1,17 @@
-import { Container, Row, Col, ListGroup, ListGroupItem, Button } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import { Trash3 } from "react-bootstrap-icons";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Favourites = () => {
   const favourites = useSelector((state) => state.favourite.list);
   const dispatch = useDispatch();
-
-  const navigate = useNavigate();
 
   return (
     <Container>
       <Row>
         <Col xs={10} className="mx-auto my-3">
           <h1>Favourites</h1>
-          <Button onClick={() => navigate("/")}>Home</Button>
         </Col>
         <Col xs={10} className="mx-auto my-3">
           <ListGroup>
